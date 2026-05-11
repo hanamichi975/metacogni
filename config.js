b4c154fec61dc521c2929541dcfdd9c4a9f98ff4
hanamichi975 <phylab9109@djshs.djsch.kr>
2026-05-12 08:28:31 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+  window.CONFIG = {
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxBc4RkzIHXVuCjXn2
+  VDibE54thpUxpUZnhni43fQBxF2KfPhi1GDURWcYwwJxpN3sl/exec",
+    SCHOOL_NAME: "대전과학고등학교",
+    APP_VERSION: "1.1.0"
+  };
