b8b0007c6bbdc820690a99e0241e012156e546d9
hanamichi975 <phylab9109@djshs.djsch.kr>
2026-05-12 08:32:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
   window.CONFIG = {
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxBc4RkzIHXVuCjXn2
-  VDibE54thpUxpUZnhni43fQBxF2KfPhi1GDURWcYwwJxpN3sl/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwRberWsW3iastVlq-EDkHGFAUeKlcEXRVs7TB_xkhw7mCnt89vuHu0pX8_jH4ImkjOJw/exec",
     SCHOOL_NAME: "대전과학고등학교",
     APP_VERSION: "1.1.0"
   };
